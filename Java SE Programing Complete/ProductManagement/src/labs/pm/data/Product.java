1f5caf471e09fee4349dd73430b3479613c958cc
Pedro Rodrigues <pedroras004@gmail.com>
2021-03-26 16:55:51 -0300

    AULA26-36

--- Java SE Programing Complete/ProductManagement/src/labs/pm/data/Product.java
+++ Java SE Programing Complete/ProductManagement/src/labs/pm/data/Product.java
@@ -18,6 +18,8 @@ package labs.pm.data;
 
 import java.math.BigDecimal;
 import static java.math.RoundingMode.HALF_UP;
+import java.util.Objects;
+import static labs.pm.data.Rating.*;
 
 /**
  *
@@ -25,10 +27,30 @@ import static java.math.RoundingMode.HALF_UP;
  */
 public class Product {
 
-    public static final BigDecimal DISCOUNT_RATE=BigDecimal.valueOf(0.1);
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
     private int id;
     private String name;
     private BigDecimal price;
+    private Rating rating;
+
+    public Product() {
+        this(0, "no name", BigDecimal.ZERO);
+    }
+
+    public Product(int id, String name, BigDecimal price, Rating rating) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public Product(int id, String name, BigDecimal price) {
+        this(id, name, price, NOT_RATED);
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
 
     public int getId() {
         return id;
@@ -50,12 +72,47 @@ public class Product {
         return price;
     }
 
-    public void setPrice(final BigDecimal price) {
-//        price = BigDecimal.ONE;
-        this.price = price;
+//    public void setPrice(final BigDecimal price) {
+////        price = BigDecimal.ONE;
+//        this.price = price;
+//    }
+    public BigDecimal getDiscount() {
+        return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
+
+    public Product applyRating(Rating newRating) {
+        return new Product(this.id, this.name, this.price, newRating);
+    }
+
+    @Override
+    public String toString() {
+        return id + ", " + name + ", " + price + ", " + getDiscount() + ", " + rating.getStars();
+    }
+
+    public boolean teste(Product p7) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+//        if(obj != null && getClass() == obj.getClass()){
+        if(obj instanceof Product){
+            final Product other = (Product) obj;
+            return this.id == other.id && Objects.equals(this.name, other.name);
+        }
+        return false;
     }
     
-    public BigDecimal getDiscount(){
-        return price.multiply(DISCOUNT_RATE).setScale(2,HALF_UP);
-    }
+    
+    
 }
